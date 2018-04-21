47005854a0f2e4846d495e09814afa6a59cb2950
Bradley Stuart Kirton <bradleykirton@gmail.com>
2018-04-21 16:10:40 +0200

    Provided overrides for the create and update methods on the user serializer to accomodate for setting and updating passwords

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,6 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 		fields = (
 			'id',
 			'username',
+			'password',
 			'email',
 			'first_name',
 			'last_name',
@@ -19,7 +20,50 @@ class UserSerializer(serializers.ModelSerializer):
 			'url'
 		)
 
-		read_only_fields = ('is_active', )
+		extra_kwargs = {
+			'password': {'write_only': True, 'style': {'input_type': 'password'}},
+			'is_active': {'read_only': True}
+		}
+
+	def create(self, validated_data: dict) -> auth_models.User:
+		"""Create the user instance
+
+		Args:
+		    validated_data: The validated serializer data
+
+		Returns:
+		    A django auth user instance
+		"""
+		# Pop the password so we can set it later
+		password = validated_data.pop('password')
+		
+		# Create a user instance
+		user_instance = auth_models.User(**validated_data)
+		user_instance.set_password(password)
+		user_instance.save()
+		
+		return user_instance
+
+	def update(self, instance: auth_models.User, validated_data: dict) -> auth_models.User:
+		"""Update the user instance
+
+		Args:
+		    instance: The user instance to be updated
+		    validated_data: The validated serializer data
+
+		Returns:
+		    A django auth user instance
+		"""
+		password = validated_data.pop('password', None)
+		
+		if password is not None:
+			instance.set_password(password)
+
+		for field, value in validated_data.items():
+			setattr(instance, field, value)
+
+		instance.save()
+		return instance
 
 
 class GroupSerializer(serializers.ModelSerializer):
