491eda4c7092e1ba28be703796adc5c4056aa303
Bradley Stuart Kirton <bradleykirton@gmail.com>
2018-04-21 07:13:46 +0200

    Added the employees models to the admin site

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from employees.models import Profile
+from employees.models import Position
+from employees.models import PositionHistory
+from employees.models import Review
 
-# Register your models here.
+
+# Register the models on the admin site
+admin.site.register(Profile)
+admin.site.register(Position)
+admin.site.register(PositionHistory)
+admin.site.register(Review)
